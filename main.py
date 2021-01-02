73328e8fdbdc6ab510b0f56b2eba2acee8bbc7f8
Przemo23 <przemek.bedelek@hotmail.de>
2021-01-02 16:54:41 +0100

    Dropdowns added to cart_template

--- main.py
+++ main.py
@@ -5,19 +5,24 @@ import uvicorn
 app = FastAPI()
 templates = Jinja2Templates(directory='templates')
 
+products = [{"name": "God of War", "count": 2, "price": 29.99, "id": 1}, \
+                {"name": "Celeste", "count": 3, "price": 9.99, "id": 2}]
+cities = ["Warszawa", "Szczecin", "Gdańsk", "Poznań", "Nowa Sól"]
+
 
 @app.get("/")
 def root(request: Request):
 
-    products = [{"name":"God of War","count":2,"price":29.99,"id":1},\
-               {"name":"Celeste","count":3,"price":9.99,"id":2}]
+    return templates.TemplateResponse("cart_template.html",
+                                      {"request": request, "message": "Hell world!", "products": products,
+                                       "cities": cities})
 
-    return templates.TemplateResponse("cart_template.html", {"request": request, "message": "Hell world!","products":products})
 
 @app.post("/add_to_cart")
 def add_to_cart(request: Request):
     id = request.forms.get("id")
     print(id)
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
