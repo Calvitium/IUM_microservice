0fa8e4920571b031ac8ea569016a151a09ffb44c
Calvitium <mateusz@marciniewicz.com>
2021-01-02 16:52:29 +0100

    DB products selection

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 import uvicorn
+import sqlite3
 
 app = FastAPI()
 templates = Jinja2Templates(directory='templates')
 
+bucket = []  # elements as a tuple of (product_id, count)
+pagination_iterator = 1
 
 @app.get("/")
 def root(request: Request):
-    products = [{"name":"God of War","count":2,"price":29.99,"id":1},\
-               {"name":"Celeste","count":3,"price":9.99,"id":2}]
+    with sqlite3.connect('db.db') as connection:
+        cursor = connection.cursor()
+        products_db = cursor.execute(f"SELECT id, name, price FROM products LIMIT 20 OFFSET {20*pagination_iterator}")
+        print(products_db)
+    products = [{
+            "name": "God of War", "count": 2, "price": 29.99, "id": 1
+         }, {
+            "name": "Celeste", "count": 3, "price": 9.99, "id": 2
+        }]
 
-    return templates.TemplateResponse("root_template.html", {"request": request, "message": "Hell world!","products":products})
+    return templates.TemplateResponse("root_template.html",
+                                      {"request": request, "message": "Hell world!", "products": products})
+
 
 @app.post("/add_to_cart")
-def add_to_cart(request: Request):
-    id = request.forms.get("id")
-    print(id)
+async def add_to_cart(request: Request, product_id: int = Form(default=-1), num_products: int = Form(default=-1)):
+    bucket.append((product_id, num_products))
+    return templates.TemplateResponse("root_template.html",
+                                      {"request": request, "message": "Dodano do koszyka", "products": products})
 
 
 if __name__ == "__main__":
