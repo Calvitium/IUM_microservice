23341b8f0bb9dc5a773353075e275dcea30edef9
Calvitium <mateusz@marciniewicz.com>
2021-01-02 17:11:06 +0100

    Bucket works

--- main.py
+++ main.py
@@ -9,27 +9,24 @@ templates = Jinja2Templates(directory='templates')
 bucket = []  # elements as a tuple of (product_id, count)
 pagination_iterator = 1
 
+
 @app.get("/")
-def root(request: Request):
+def root(request: Request, message: str = "Wybierz sobie jakiś produkt do zakupienia"):
     with sqlite3.connect('db.db') as connection:
         cursor = connection.cursor()
-        products_db = cursor.execute(f"SELECT id, name, price FROM products LIMIT 20 OFFSET {20*pagination_iterator}")
-        print(products_db)
-    products = [{
-            "name": "God of War", "count": 2, "price": 29.99, "id": 1
-         }, {
-            "name": "Celeste", "count": 3, "price": 9.99, "id": 2
-        }]
+        products_db = cursor.execute(
+            f"SELECT id, name, price FROM products LIMIT 20 OFFSET {20 * pagination_iterator}").fetchall()
+
+    products_dictionary = [{"id": product[0], "name": product[1], "price": product[2]} for product in products_db]
 
     return templates.TemplateResponse("root_template.html",
-                                      {"request": request, "message": "Hell world!", "products": products})
+                                      {"request": request, "message": message, "products": products_dictionary})
 
 
 @app.post("/add_to_cart")
 async def add_to_cart(request: Request, product_id: int = Form(default=-1), num_products: int = Form(default=-1)):
     bucket.append((product_id, num_products))
-    return templates.TemplateResponse("root_template.html",
-                                      {"request": request, "message": "Dodano do koszyka", "products": products})
+    root(request, message="Dodano do koszyka")
 
 
 if __name__ == "__main__":
